894de2ef22bdca9c1426105d255b75c545060d7f
Aixa Filsinger <macafil99@gmail.com>
2023-06-02 13:10:22 -0300

    agregamos un nuevo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import logo from './assets/logosimpson.png'
+import { Button, Container } from 'react-bootstrap';
+import Frase from './components/Frase';
 
 function App() {
   
   return (
     <>
-     
+    <Container className='text-center my-5'>
+        <img src={logo} alt="logo de los simpson"  className='w-50'/>
+        <Frase></Frase>
+        <Button variant='warning'>Obtener frase</Button>
+    </Container>
     </>
   )
 }
